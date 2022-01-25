a423567f8a59ea41afe877e8888d53ba3658b550
Hitesh.Balegar <hitesh.balegar@betsol.com>
2022-01-25 18:43:58 +0530

    fixed timezones

--- VaccinationService/models.py
+++ VaccinationService/models.py
@@ -1,4 +1,4 @@
-from datetime import date
+from datetime import datetime
 from django.db import models
 from UserSupportService.models import User
 # Create your models here.
@@ -6,5 +6,5 @@ from UserSupportService.models import User
 class RegisterMember(models.Model):
     id= models.AutoField(primary_key=True)
     user = models.ForeignKey(User,on_delete=models.CASCADE)
-    DateTimeRegistered = models.DateTimeField(default=date.today)
+    DateTimeRegistered = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=150,null=False)
